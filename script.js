c608ef85942c4622fe88a9fd137bb2a15f1e2d8e
Ben Newton <bennewton91@gmail.com>
2024-10-27 20:25:37 +0800

    added layout and some calculator functionality

---
+++ script.js
@@ -0,0 +1,48 @@
+let buttons = [];
+
+
+let current_number = []; 
+
+function create_button(value)
+{
+    let div = document.createElement('div');
+    div.setAttribute('id', value);
+    div.setAttribute('class', 'button');
+    div.innerHTML = value;
+    
+    div.addEventListener('click', function(){
+        get_number(value);
+        put_on_screen();
+    })
+
+    return div;
+}
+
+for (let i = 9; i >= 0; i--)
+{
+    buttons.push(create_button(i));
+}
+
+let number_container = document.getElementById('numbers');
+for (let i = 0; i < buttons.length; i++)
+{
+    number_container.appendChild(buttons[i]);
+}
+
+function get_number(button_value)
+{
+    current_number.push(button_value);
+}
+
+screen=document.getElementById('screen');
+
+function put_on_screen()
+{
+    let to_add = ''
+    for (let i = 0; i < current_number.length; i++)
+    {
+        to_add+=current_number[i];
+    }
+    
+    screen.innerHTML = to_add;
+}
