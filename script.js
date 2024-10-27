8fb0196d2a3355ea87f36c4d91c76c918c9aa611
Ben Newton <bennewton91@gmail.com>
2024-10-27 20:37:14 +0800

    made look ok

--- script.js
+++ script.js
@@ -18,10 +18,14 @@ function create_button(value)
     return div;
 }
 
+
 for (let i = 9; i >= 0; i--)
 {
     buttons.push(create_button(i));
 }
+
+buttons.push(create_button('.'));
+
 
 let number_container = document.getElementById('numbers');
 for (let i = 0; i < buttons.length; i++)
@@ -31,18 +35,25 @@ for (let i = 0; i < buttons.length; i++)
 
 function get_number(button_value)
 {
-    current_number.push(button_value);
+    if (current_number.length <= 8)
+    {
+        current_number.push(button_value);
+    }
+    else
+    {
+        alert("Number too large!");
+    }
+    
 }
 
 screen=document.getElementById('screen');
 
 function put_on_screen()
 {
-    let to_add = ''
+    let to_add = '';
     for (let i = 0; i < current_number.length; i++)
     {
         to_add+=current_number[i];
     }
-    
     screen.innerHTML = to_add;
 }
